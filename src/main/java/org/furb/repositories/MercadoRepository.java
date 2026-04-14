2ecafba80513e6cae21813627534f4cd758250a0
lucaseduardotestoni <testonilucas015@gmail.com>
2026-04-14 18:52:11 -0300

    fix: rename aplication-prod.properties and update repository configs

---
+++ src/main/java/org/furb/repositories/MercadoRepository.java
@@ -0,0 +1,14 @@
+package org.furb.repositories.model;
+
+
+import org.furb.model.Mercado;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface MercadoRepository extends JpaRepository<Mercado, Long> {
+    Optional<Mercado> findByCnpj(String cnpj);
+    boolean existsByCnpj(String cnpj);
+}
