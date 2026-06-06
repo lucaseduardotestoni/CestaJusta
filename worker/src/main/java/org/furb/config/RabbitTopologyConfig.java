b998109633a39d17681dd7e5e89bd1fde7ad97a4
lucaseduardotestoni <testonilucas015@gmail.com>
2026-06-06 17:24:44 -0300

    fix(worker): provide ObjectMapper bean for message listeners

--- worker/src/main/java/org/furb/config/RabbitTopologyConfig.java
+++ worker/src/main/java/org/furb/config/RabbitTopologyConfig.java
@@ -1,5 +1,7 @@
 package org.furb.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.furb.messaging.contract.RoutingKeys;
 import org.springframework.amqp.core.Binding;
 import org.springframework.amqp.core.BindingBuilder;
@@ -8,11 +10,18 @@ import org.springframework.amqp.core.QueueBuilder;
 import org.springframework.amqp.core.TopicExchange;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class RabbitTopologyConfig {
+
+    @Bean
+    @ConditionalOnMissingBean
+    public ObjectMapper objectMapper() {
+        return new ObjectMapper().registerModule(new JavaTimeModule());
+    }
 
     @Bean
     public MessageConverter jsonMessageConverter() {
