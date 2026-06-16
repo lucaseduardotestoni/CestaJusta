1f066100ad848e9075cb5032b17f4f4e585e4490
lucaseduardotestoni <testonilucas015@gmail.com>
2026-06-15 21:29:52 -0300

    feat(auth): add RefreshTokenRepository

---
+++ core/src/main/java/org/furb/repositories/RefreshTokenRepository.java
@@ -0,0 +1,14 @@
+package org.furb.repositories;
+
+import org.furb.model.RefreshToken;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long> {
+    Optional<RefreshToken> findByTokenHash(String tokenHash);
+    List<RefreshToken> findByFamilyId(String familyId);
+}
